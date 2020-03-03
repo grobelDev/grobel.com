ace8ce235ee8f62323e83b8087ac13e09d946a43
grobelDev <56764278+grobelDev@users.noreply.github.com>
2020-03-03 13:18:06 -0800

    testing a fully functioning example

--- server/index.js
+++ server/index.js
@@ -1,61 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const markdown = require('./markdown.js');
-const { getExample, getHn } = require('./example.js');
+'use strict';
 
+const { screenshot } = require('./screenshot.js');
+
+const express = require('express');
+const puppeteer = require('puppeteer');
 const app = express();
 
-let env = process.env.NODE_ENV || 'development';
+app.use(screenshot);
 
-// if (env === 'development') {
-//   app.use(cors());
-// } else {
-//   let corsOptions = {
-//     origin: 'https://grobel.dev',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   };
-
-//   app.use(cors(corsOptions));
-// }
-
-app.use(cors());
-
-app.get('/', async (req, res) => {
-  try {
-    // let path = 'articles';
-    // let files = await markdown.getFiles(path);
-    // console.log(files);
-    res.status(200).send('hello world');
-  } catch (err) {
-    console.log(err);
-    res.status(400).send(err);
-  }
+const server = app.listen(process.env.PORT || 8080, err => {
+  if (err) return console.error(err);
+  const port = server.address().port;
+  console.info(`App listening on port ${port}`);
 });
 
-app.get('/testing', async (req, res) => {
-  try {
-    // let path = 'articles';
-    // let files = await markdown.getFiles(path);
-    // console.log(files);
-    let exampleResult = await getExample();
-
-    res.set('Content-Type', 'image/jpeg; charset=UTF-8');
-    res.status(200).send(exampleResult);
-  } catch (err) {
-    console.log(err);
-    res.status(400).send(err);
-  }
-});
-
-app.listen(8080, err => {
-  console.log('Listening on port 8080');
-});
-
-// async function debug() {
-//   //   return await markdown.getFileList();
-//   let path = 'articles';
-//   return await markdown.getFileMeta(path);
-// }
-
-// console.log(await debug());
+module.exports = app;
