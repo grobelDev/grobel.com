cc9f1a9195e13111c3d02c5b42584fe96b2e4d52
grobelDev <56764278+grobelDev@users.noreply.github.com>
2020-03-05 12:24:22 -0800

    made images scale better

--- server/screenshot.js
+++ server/screenshot.js
@@ -9,21 +9,25 @@ const puppeteer = require('puppeteer');
  * @param {Object} res Cloud Function response context.
  */
 exports.screenshot = async (req, res) => {
-    const url = req.query.url;
-  
-    if (!url) {
-      return res.send('Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>');
-    }
-  
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
-    const imageBuffer = await page.screenshot();
-    browser.close();
-  
-    res.set('Content-Type', 'image/png');
-    res.send(imageBuffer);
-  };
-  
+  const url = req.query.url;
+
+  if (!url) {
+    return res.send(
+      'Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>'
+    );
+  }
+
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox']
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 800, height: 800, deviceScaleFactor: 2 });
+  await page.goto(url, {
+    waitUntil: 'networkidle2'
+  });
+  const imageBuffer = await page.screenshot();
+  browser.close();
+
+  res.set('Content-Type', 'image/png');
+  res.send(imageBuffer);
+};
